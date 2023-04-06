8f8b78393c40265dbd8fe47824e38a50bbaaebe2
Lasse Schmidt <l.s10600@gmail.com>
2023-04-06 12:06:04 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Oenskeportalen_APP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Oenskeportalen_APP;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
